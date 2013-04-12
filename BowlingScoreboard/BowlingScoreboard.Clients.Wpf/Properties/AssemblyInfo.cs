6f5ebccf899efed86dc5fbe1a0ccd2c01e350dc3
Francis Brito <fr.br94@gmail.com>
2013-04-12 17:19:19 -0400

    Revision #1

--- BowlingScoreboard/BowlingScoreboard.Clients.Wpf/Properties/AssemblyInfo.cs
+++ BowlingScoreboard/BowlingScoreboard.Clients.Wpf/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BowlingScoreboard.Clients.Wpf")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bowling Scoreboard Application")]
+[assembly: AssemblyDescription("An app for bowling consultants")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BowlingScoreboard.Clients.Wpf")]
+[assembly: AssemblyCompany("IDS317G1")]
+[assembly: AssemblyProduct("Bowling Scoreboard Application")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
