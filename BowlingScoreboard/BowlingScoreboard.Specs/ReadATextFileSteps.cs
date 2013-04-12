71cb7e5b844534c924c1f7c06af66e6531c7d379
Francis Brito <fr.br94@gmail.com>
2013-04-12 09:32:34 -0400

    Created tests for read a text file feature

--- BowlingScoreboard/BowlingScoreboard.Specs/ReadATextFileSteps.cs
+++ BowlingScoreboard/BowlingScoreboard.Specs/ReadATextFileSteps.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using TechTalk.SpecFlow;
+using System.Net;
 
 namespace BowlingScoreboard.Specs
 {
@@ -8,84 +11,121 @@ namespace BowlingScoreboard.Specs
     {
         private string _fileName;
         private string _filePath;
+
+        private IFileReader _fileReader;
         private string _networkShareHostName;
         private string _internetServerName;
+
         [Given(@"I want to load a file called ""(.*)""")]
         public void GivenIWantToLoadAFileCalled(string fileName)
         {
-            ScenarioContext.Current.Pending();
+            _fileName = fileName;
         }
 
         [Given(@"I think its located at the path ""(.*)""")]
         public void GivenIThinkItsLocatedAtThePath(string filePath)
         {
-            ScenarioContext.Current.Pending();
+            _filePath = filePath;
         }
 
         [Given(@"it exists in my local file system")]
         public void GivenItExistsInMyLocalFileSystem()
         {
-            ScenarioContext.Current.Pending();
+            // NOTE:
+            // I'm running two implicit steps here.
+            // I tell the application I'll be reading from the local file system.
+            // AND I tell the application the file exists.
+            // This should be seperated into two steps just like other scenarios.
+            _fileReader = new LocalFileReader();
+
+            var fullPath = string.Format("{0}{1}", _filePath, _fileName);
+
+            Assert.IsTrue(File.Exists(fullPath));
         }
 
         [When(@"I press load")]
         public void WhenIPressLoad()
         {
-            ScenarioContext.Current.Pending();
+            var fullPath = string.Format("{0}{1}", _filePath, _fileName);
+
+            _fileReader.LoadFile(fullPath);
         }
 
         [Then(@"the application should load the file")]
         public void ThenTheApplicationShouldLoadTheFile()
         {
-            ScenarioContext.Current.Pending();
+            Assert.IsTrue(_fileReader.FileReadStatus == FileReadStatus.Loaded);
         }
 
         [Given(@"it doesnt exists in my local file system")]
         public void GivenItDoesntExistsInMyLocalFileSystem()
         {
-            ScenarioContext.Current.Pending();
+            _fileReader = new LocalFileReader();
+
+            var fullPath = string.Format("{0}{1}", _filePath, _fileName);
+
+            Assert.IsFalse(File.Exists(fullPath));
         }
 
         [Then(@"the application should notify me the file doesnt exists")]
         public void ThenTheApplicationShouldNotifyMeTheFileDoesntExists()
         {
-            ScenarioContext.Current.Pending();
+            Assert.IsTrue(_reader.FileReadStatus == FileReadStatus.NotFound);
         }
 
         [Given(@"its at a network share host called ""(.*)""")]
         public void GivenItsAtANetworkShareHostCalled(string networkShareHostName)
         {
-            ScenarioContext.Current.Pending();
+            _networkShareHostName = networkShareHostName;
+            _fileReader = new NetworkShareFileReader(networkShareHostName);
         }
 
         [Given(@"it exists at the network share host")]
         public void GivenItExistsAtTheNetworkShareHost()
         {
-            ScenarioContext.Current.Pending();
+            var fullPath = string.Format("//{0}{1}{2}", _networkShareHostName, _filePath, _fileName);
+
+            Assert.IsTrue(File.Exists(fullPath));
         }
 
         [Given(@"it doesnt exists at the network share host")]
         public void GivenItDoesntExistsAtTheNetworkShareHost()
         {
-            ScenarioContext.Current.Pending();
+            var fullPath = string.Format("//{0}{1}{2}", _networkShareHostName, _filePath, _fileName);
+
+            Assert.IsFalse(File.Exists(fullPath));
         }
 
         [Given(@"its at an internet server called ""(.*)""")]
         public void GivenItsAtAnInternetServerCalled(string internetServerName)
         {
-            ScenarioContext.Current.Pending();
+            _internetServerName = internetServerName;
+
+            _fileReader = new InternetFileReader(internetServerName);
         }
 
         [Given(@"it exists at the internet server")]
         public void GivenItExistsAtTheInternetServer()
         {
-            ScenarioContext.Current.Pending();
+            var uri = string.Format("http://{0}{1}{2}", _internetServerName, _filePath, _fileName);
+
+            var request = (HttpWebRequest) WebRequest.Create(uri);
+            var response = (HttpWebResponse) request.GetResponse();
+
+            // It should be a 200 (OK)
+            Assert.IsTrue(response.StatusCode == HttpStatusCode.OK);
         }
 
         [Given(@"it doesnt exists at the internet server")]
         public void GivenItDoesntExistsAtTheInternetServer()
         {
-            ScenarioContext.Current.Pending();
+            var uri = string.Format("http://{0}{1}{2}", _internetServerName, _filePath, _fileName);
+
+            var request = (HttpWebRequest)WebRequest.Create(uri);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            // It should be a 404 (Not Found)
+            Assert.IsTrue(response.StatusCode == HttpStatusCode.NotFound);
         }
 
     }
